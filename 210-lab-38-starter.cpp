e30808686e1ea69166d0c4ca79d1162b7d6ec219
ambucha <ambuchatskiy@gmail.com>
2025-11-29 18:07:05 -0800

    last commit before i needa do something, im gonna needa finish this later

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -28,6 +28,8 @@ void load(const string &fName, IntBinaryTree &tree){
     cout << count << " codes loaded in" << endl;
 }
 
+void menu();
+
 
 int main() {
     // test it
@@ -39,7 +41,48 @@ int main() {
     // create the bst
     load(file,tree);
 
-    //display
-    tree.displayInOrder();
-    return 0;
+    int choice = -1;
+
+    // variables for codes and temp codes for the modifier part
+    string code;
+    string tempcode;
+
+    // alr now i needa make the menu for milestone 4
+    do {
+        // first lets just make a function to call that shows all the options
+        menu();
+        cin >> choice;
+        cin.ignore();
+
+        // now lets flesh out the choices
+        switch (choice){
+            case 1:
+                cout << "Enter code to add: ";
+                getline(cin, code);
+                // now we needa check if the code already exists before adding it
+                if(tree.searchNode(code)){
+                    cout << "Not adding a duplicate" << endl;
+                }
+                else{
+                    tree.insertNode(code);
+                    cout << "Code inserted" << endl;
+                }
+                break;
+                
+        }
+
+    } while(choice != 0);
+}
+
+
+void menu(){
+    cout << endl << "-----BST MENU-----" << endl;
+    cout << "1. Add code" << endl;
+    cout << "2. Delete code" << endl;
+    cout << "3. Search for code" << endl;
+    cout << "4. Modify codes" << endl;
+    // display all codes in order wasnt listed in the neededd menu options but i feel like it should be here lowkey
+    cout << "5. Display all codes in order" << endl;
+    cout << "0. Exit" << endl;
+    cout << "Enter choice: " << endl;
 }
