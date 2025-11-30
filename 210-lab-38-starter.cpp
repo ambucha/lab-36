f9d206bb27997fe6f69e098c207b62ca2fb967d4
ambucha <ambuchatskiy@gmail.com>
2025-11-29 17:57:03 -0800

    milestone 3

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "IntBinaryTree.h"
 using namespace std;
+
+// i needa make a function to fill up the tree using the given codes.txt file
+void load(const string &fName, IntBinaryTree &tree){
+    ifstream file(fName);
+
+    // check if the file was openned to begin with
+    if(!file){
+        cout << "ERROR: file couldnt be opened " << endl;
+        return;
+    }
+
+    // create variables for each line, as well as a counter to make suer all the coeds were laoded in
+    string code;
+    int count = 0;
+
+    while(getline(file, code)){
+        if(!code.empty()) {
+            tree.insertNode(code);
+            count++;
+        }
+    }
+
+    cout << count << " codes loaded in" << endl;
+}
+
 
 int main() {
     // test it
     IntBinaryTree tree;
-    tree.insertNode("manga");
-    tree.insertNode("maracuja");
+
+    // variable for the filename
+    string file = "codes.txt";
+
+    // create the bst
+    load(file,tree);
+
+    //display
     tree.displayInOrder();
     return 0;
 }
