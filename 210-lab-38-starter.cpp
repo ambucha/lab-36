f8540059d5eb010216f45560760a5f1983e6792b
ambucha <ambuchatskiy@gmail.com>
2025-11-29 17:46:26 -0800

    milestone 2

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main() {
-    
-
+    // test it
+    IntBinaryTree tree;
+    tree.insertNode("manga");
+    tree.insertNode("maracuja");
+    tree.displayInOrder();
     return 0;
 }
